8795dfa73c5d9fc25cda1dea4f75c92d5a11d335
Cesar Rodriguez Toledo <cesar.2010.11@gmail.com>
2017-04-18 09:55:46 -0500

    Arreglos multidimensionales

---
+++ PHP1/Clase5/hoja3.php
@@ -0,0 +1,13 @@
+<?php
+/*Arreglos multidimensionales*/
+
+$numeros = array(
+		array(1, 2, 3),		//Indice 0
+		array(4, 5, 6),		//Indice 1
+		array(7, 8, 9)		//Indice 2
+	//Indices 0--1--2
+	);
+
+echo "El elemento [1][1] es= {$numeros[1][1]} <br>";
+
+?>
